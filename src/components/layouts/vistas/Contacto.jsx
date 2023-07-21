149242e57f34cfcf9e3aec3a20b41bf697d43149
ValFJ2023 <valentina.betancur@fundacion-jala.org>
2023-07-21 18:23:14 -0500

    terminada

--- src/components/layouts/vistas/Contacto.jsx
+++ src/components/layouts/vistas/Contacto.jsx
@@ -1,9 +1,6 @@
 import Principal from "../../helpers/Principal";
 
 const Contacto = () => {
-    function limpiar(){
-        
-    }
   return (
     <section>
       <Principal />
@@ -12,12 +9,13 @@ const Contacto = () => {
         <form className="formulario">
           <h2>Dejame tus datos para contactarte pronto.</h2>
           <label>Nombre</label>
-          <input type="text" placeholder="Valentina Betancur" required />
+          <input type="text" placeholder="Valentina Betancur" id="nombre" required />
           <label>Telefono</label>
           <input type="number" placeholder="1234567890" required />
           <label>Correo</label>
           <input type="email" placeholder="alguien@correo.com" required />
-          <button type="reset">Enviar</button>
+          <button>Enviar</button>
+          
         </form>
       </section>
     </section>
